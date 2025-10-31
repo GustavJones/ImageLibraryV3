ec918927b2debe8329c7647ad7f6e82903ab0b0d
GustavJones <gustavjones3@gmail.com>
2025-10-31 23:18:35 +0200

    Fix multi config generators from separating executables from data files

--- data/build.cmake
+++ data/build.cmake
@@ -14,12 +14,12 @@ add_custom_target(ImageLibrary-Website-Dependencies ALL COMMAND ${NPM_EXECUTABLE
 add_custom_target(ImageLibrary-Website ALL COMMAND ${NPM_EXECUTABLE} run build WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/data COMMENT "Building ImageLibrary-Website" DEPENDS ImageLibrary-Website-Dependencies)
 
 foreach(HTML_FILE ${HTML_FILES})
-  add_custom_command(TARGET ImageLibrary-Website POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy ${HTML_FILE} ${CMAKE_BINARY_DIR}/bin/data)
+  add_custom_command(TARGET ImageLibrary-Website POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy ${HTML_FILE} ${IMAGE_LIBRARY_BINARY_RUNTIME_DIR}/data)
 endforeach()
 
 foreach(SCRIPT_FILE ${SCRIPT_FILES})
-  add_custom_command(TARGET ImageLibrary-Website POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy ${SCRIPT_FILE} ${CMAKE_BINARY_DIR}/bin/data/scripts)
+  add_custom_command(TARGET ImageLibrary-Website POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy ${SCRIPT_FILE} ${IMAGE_LIBRARY_BINARY_RUNTIME_DIR}/data/scripts)
 endforeach()
 
-add_custom_command(TARGET ImageLibrary-Website POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_directory ${STYLE_DIRECTORY} ${CMAKE_BINARY_DIR}/bin/data/style)
-add_custom_command(TARGET ImageLibrary-Website POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_directory ${ASSETS_DIRECTORY} ${CMAKE_BINARY_DIR}/bin/data/assets)
+add_custom_command(TARGET ImageLibrary-Website POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_directory ${STYLE_DIRECTORY} ${IMAGE_LIBRARY_BINARY_RUNTIME_DIR}/data/style)
+add_custom_command(TARGET ImageLibrary-Website POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_directory ${ASSETS_DIRECTORY} ${IMAGE_LIBRARY_BINARY_RUNTIME_DIR}/data/assets)
